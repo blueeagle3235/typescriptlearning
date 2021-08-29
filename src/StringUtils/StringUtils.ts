bfc09a399eb0e1e13c724f06644ff532793e0a91
givemepower8 <givemepower8@gmail.com>
2021-08-29 11:23:56 -0700

    minor

--- src/StringUtils/StringUtils.ts
+++ src/StringUtils/StringUtils.ts
@@ -1,3 +1,10 @@
-class StringUtils{
-    
+class StringUtils {
+    constructor() { }
+
+    /** an example function does console.log */
+    public log(message: string) {
+        console.log(message);
+    }
 }
+
+export default StringUtils;
--- src/playground/first.ts
+++ src/playground/first.ts
@@ -1,7 +1,11 @@
 import sum from "./sum"
+import StringUtils from "../StringUtils/StringUtils"
 
-let c:number=0;
-c=sum(1,3);
+let c: number = 0;
+c = sum(1, 3);
 console.log(`hello world! ${c}`);
+
+let util: StringUtils = new StringUtils();
+util.log("test");
 
 //ts only cool code
