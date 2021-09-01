de4e38d7d053602e1888a9a496d5bf8d341f1fa3
Wang, Wei <wei.wang@ia.ca>
2021-09-01 11:47:40 -0700

    minor

---
+++ src/StringUtils/StringUtils.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var StringUtils = /** @class */ (function () {
+    function StringUtils() {
+    }
+    /** an example function does console.log */
+    StringUtils.prototype.log = function (message) {
+        console.log(message);
+    };
+    return StringUtils;
+}());
+exports["default"] = StringUtils;
