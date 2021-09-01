d3b6a82558374b7e3bee8546801c3aa8c9398371
Wang, Wei <wei.wang@ia.ca>
2021-09-01 14:32:08 -0700

    minor

---
+++ src/Basics/array.ts
@@ -0,0 +1,7 @@
+let numbers: number[] = [2, 4, 6, 8];
+console.log(numbers);
+
+for (var i = 0; i < numbers.length; i++) {
+  numbers[i] = i * 2;
+}
+console.log(numbers);
---
+++ src/Basics/date.ts
@@ -0,0 +1,2 @@
+const d: Date = new Date();
+console.log(d.getTime()); //1630522759764, the getTime() method returns the number of milliseconds between midnight of January 1, 1970 and the specified date, a.k.a, the Unix epoch (or Unix time or POSIX time or Unix timestamp)
