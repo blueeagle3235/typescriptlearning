99b51dc88b131a7a6cf55fb8b9e53376d28eb6a8
Wang, Wei <wei.wang@ia.ca>
2021-09-01 11:55:03 -0700

    basics

---
+++ src/Basics/number.ts
@@ -0,0 +1,6 @@
+let int: number = 6;
+let decimal: number = 6.1;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+//let big: bigint = 100n; //https://stackoverflow.com/questions/57560501/bigint-literals-are-not-available-when-targeting-lower-than-esnext
