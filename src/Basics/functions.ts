48cb7a803f67c4f12a95871dc583381cfe628abb
Wang, Wei <wei.wang@ia.ca>
2021-09-02 09:00:45 -0700

    minor

--- src/Basics/functions.ts
+++ src/Basics/functions.ts
@@ -1,7 +1,42 @@
 // https://www.typescriptlang.org/play?noImplicitAny=false&q=256#example/functions
 
 // declare a function
+// usually function has name, arrow function can be anonymous
 
-// parameters and arguments
+// #region parameters and arguments
+
+// default values of parameters
+
+// parameters with spread syntax
+
+// #endregion
 
 // returns or void
+
+// #region arrow functions
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+// #endregion
+
+// #region callback functions
+// callback functions as parameters
+// https://developer.mozilla.org/en-US/docs/Glossary/Callback_function
+
+function greeting(name: string) {
+  console.log("Hello " + name);
+}
+
+function processUserInput(name: string, callback: any) {
+  // callback can have access to local varibles
+  let localVariable: string = ", welcome onboard!";
+  callback(name + localVariable);
+}
+
+// use named function
+processUserInput("user", greeting);
+// use anonymous function
+processUserInput("user", (c: string) => {
+  console.log("Hello! " + c);
+});
+
+// #endregion
