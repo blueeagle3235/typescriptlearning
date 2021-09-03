acc9a832f7988a3c0eb73b6d7181ca512b960d32
Wang, Wei <wei.wang@ia.ca>
2021-09-03 14:04:49 -0700

    minor

---
+++ src/Basics/map_reduce.ts
@@ -0,0 +1,3 @@
+import { users } from "./sampleData";
+
+console.log(users);
---
+++ src/playground/axios.ts
@@ -0,0 +1,26 @@
+import https from "https";
+import axios, { AxiosResponse } from "axios";
+
+//https://github.com/axios/axios
+//https://jsonplaceholder.typicode.com
+
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
+// axios("http://google.com/").then(function (response: any) {
+//   console.log(response.data);
+// });
+
+axios
+  .get("https://jsonplaceholder.typicode.com/posts", { httpsAgent })
+  .then((response: AxiosResponse<any>) => {
+    // handle success
+    //console.log(response.data);
+  })
+  .catch((error) => {
+    // handle error
+    console.log(error);
+  })
+  .then(() => {
+    // always executed
+    console.log("done");
+  });
