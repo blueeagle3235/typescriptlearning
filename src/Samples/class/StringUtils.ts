27d8370a7037f6e276689daf632c61513a54bad3
Wang, Wei <wei.wang@ia.ca>
2021-09-04 07:57:28 -0700

    reorg files

---
+++ src/Samples/class/StringUtils.ts
@@ -0,0 +1,10 @@
+class StringUtils {
+  constructor() {}
+
+  /** an example function does console.log */
+  public log(message: string) {
+    console.log(message);
+  }
+}
+
+export default StringUtils;
