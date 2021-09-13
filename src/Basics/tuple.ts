36c7f12b3be94fcbac30e1b78da26d9b23fa85fe
Wang, Wei <wei.wang@ia.ca>
2021-09-12 21:52:05 -0700

    minor

--- src/Basics/tuple.ts
+++ src/Basics/tuple.ts
@@ -1,3 +1,5 @@
+// it's good for materializing data from database
+// but it can be done using object array and object destructuring
 let hat1: [string, number] = ["bonnet", 100];
 let hat2: [string, number] = ["baseball cap", 10];
 let hats: [string, number][] = [hat1, hat2];
@@ -5,3 +7,5 @@ hats.forEach((hat) => {
   let [hatName, hatPrice] = hat;
   console.log(hatName, hatPrice);
 });
+
+// ofter the case, tuple is handy to return multiple values
