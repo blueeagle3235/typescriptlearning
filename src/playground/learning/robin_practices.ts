2d64b3e8fbec076ad5e8d738afab45dcd10c34b7
givemepower8 <givemepower8@gmail.com>
2022-04-18 10:32:29 -0700

    minor changes

--- src/playground/learning/robin_practices.ts
+++ src/playground/learning/robin_practices.ts
@@ -1,5 +1,17 @@
 function sum(a:number,b:number):number{
+    console.log("in sum");
     return a+b;
 }
 
-console.log(sum(1111,22222));
+let result=sum(11,22);
+
+console.log(result);
+
+
+function greet(name:string):void{
+    console.log("hello "+ name);
+}
+
+greet("Aaron");
+
+greet("Robin");
