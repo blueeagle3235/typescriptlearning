49a8557fa4c770dc24d217688f42b09e352e812b
givemepower8 <givemepower8@gmail.com>
2022-04-30 09:42:39 -0700

    minor

--- src/Canvas/Ball/Ball.ts
+++ src/Canvas/Ball/Ball.ts
@@ -1,6 +1,9 @@
+import { VoidExpression } from "typescript";
+
 export class Ball {
 
     private context: CanvasRenderingContext2D;
+    private canvas: HTMLCanvasElement;
 
     public x: number = 0;
     public y: number = 0;
@@ -8,22 +11,50 @@ export class Ball {
     private color: string = 'orange';
     public label: string = "";
 
+    private stepInterval: number = 0;
+
+    private dx: number = 2;
+    private dy: number = 1;
+
     constructor(x: number, y: number, label: string = "") {
-        let canvas = document.getElementById('canvas') as HTMLCanvasElement;
-        let context = canvas.getContext("2d");
+        this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        let context = this.canvas.getContext("2d");
         this.context = context!;
         this.x = x;
         this.y = y;
-        this.label=label;
+        this.label = label;
     }
 
     public draw(): void {
+        //this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.context.beginPath();
         this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
         this.context.closePath();
         this.context.fillStyle = this.color;
         this.context.fill();
-        this.context.font = "12px Arial";
+        this.context.font = "12pt Arial";
         this.context.strokeText(this.label, this.x - 5, this.y + 5);
     }
+
+    public move(): void {
+        this.stepInterval = window.setInterval(() => this.onEachStep(), 1000 / 60); // 60 fps
+
+    }
+
+    public stop(): void {
+        window.clearInterval(this.stepInterval); // 60 fps
+    }
+
+    private onEachStep(): void {
+        if (this.x + this.dx > this.canvas.width - this.radius || this.x + this.dx < this.radius) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.dy > this.canvas.height - this.radius || this.y + this.dy < this.radius) {
+            this.dy = -this.dy;
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw();
+    }
+
 };
--- src/Canvas/Ball/World.ts
+++ src/Canvas/Ball/World.ts
@@ -3,23 +3,22 @@ import { Turtle } from "./Turtle"
 
 // let ball1 = new Ball(35, 35, "1");
 // ball1.draw();
-
+// ball1.move();
 // let ball2 = new Ball(350, 35,"2");
 // ball2.draw();
 
-for (let i=25;i<=500;i+=50)
-{
-    for (let j=25;j<=500;j+=50)
-    {
-        let x=(i-25)/50;
-        let y=(j-25)/50;
+for (let i = 25; i <= 500; i += 50) {
+    for (let j = 25; j <= 500; j += 50) {
+        let x = (i - 25) / 50;
+        let y = (j - 25) / 50;
         let ball = new Ball(i, j, `${x},${y}`);
         ball.draw();
+        //ball.move();
     }
 }
 
-let turtle = new Turtle( 250, 250);
-turtle.setPen(true);
+// let turtle = new Turtle( 250, 250);
+// turtle.setPen(true);
 // turtle.savePos();
 // turtle.setWidth(20);
 // for (let i = 0; i < 36; i++) {
@@ -52,4 +51,4 @@ turtle.setPen(true);
 //     turtle.turnRight(90);
 // }
 
-turtle.runAll();
+//turtle.runAll();
