29be3d4d8bbe8a7e895f5713cc9ea45b7ab1580f
givemepower8 <givemepower8@gmail.com>
2022-05-25 10:20:19 -0700

    minor

---
+++ src/Canvas/Ball/ClassDoodle.ts
@@ -0,0 +1,10 @@
+export class Calculator {
+    public add(number1: number, number2: number): number {
+        return number1 + number2;
+    }
+}
+
+let calculator = new Calculator();
+let result = calculator.add(1, 1);
+console.log(result);
+
