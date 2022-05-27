56ae2b6c8721b440cb7a6b9bfd68e637b3681199
givemepower8 <givemepower8@gmail.com>
2022-05-26 21:16:22 -0700

    minor

--- src/Robin/ClassDoodle.ts
+++ src/Robin/ClassDoodle.ts
@@ -4,7 +4,39 @@ export class Calculator {
     }
 }
 
-let calculator = new Calculator();
-let result = calculator.add(1, 1);
-console.log(result);
+export class Dog{
+    private HP : number=100;
+
+    public ShowHP():void{
+        console.log(this.HP);
+    }
+
+    public GetHit():void{
+        console.log("woof, woof!");
+        if  (this.HP>0){
+            this.HP = this.HP-1;
+        }
+    }
+    public Sleep():void{
+        console.log("zzz...");
+        if (this.HP<100){
+            this.HP = this.HP+1;
+        }
+    }
+}
+
+let d1 = new Dog();
+let d2 = new Dog();
+d1.ShowHP();
+d1.GetHit();
+d1.ShowHP();
+d1.Sleep();
+
+//d1.HP=50000;
+//console.log(d1.HP)
+
+
+// let calculator = new Calculator();
+// let result = calculator.add(1, 1);
+// console.log(result);
 
